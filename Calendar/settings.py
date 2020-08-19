f29df0de64b8f589a30589596914f1a44f281285
aibba19 <andrius96@hotmail.it>
2020-08-19 18:29:09 +0200

    add allowed host correction

--- Calendar/settings.py
+++ Calendar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hk7%6c@omgy9qdqoup96&s9_w^pmx3^%=m+kr*d+2eh@otz6cc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
