f1527c9dca03eb5217ee02ae07e20f12f67a7911
aibba19 <andrius96@hotmail.it>
2020-09-30 19:41:30 +0200

    add readme

---
+++ readme.h
@@ -0,0 +1,15 @@
+1 - pip3 install -r requirements.txt
+
+2 - python3 manage.py migrate and makemigrations
+
+3 - python3 manage.py collectstatic
+
+4 - python3 manage.py createsuperuser
+
+5- Settare Path in /Calendar/__init__.py al path totale dove si trova il file cal/mqtt_iot (ex: /home/ubuntu/CalendarEsit/cal/ )
+
+6 - Cambiare indirizzo host a localhost in caso di utilizzo in locale in cal/mqtt_iot  
+
+
+
+
